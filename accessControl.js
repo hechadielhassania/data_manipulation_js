c4b84cd522108b6d85105f885ae2bc4547c18898
Hechadi El Hassania <hechadihassaia@gmail.com>
2024-03-14 10:29:25 -0400

    access control exercice

---
+++ accessControl.js
@@ -0,0 +1,31 @@
+// Declare a variable called num
+let num = 10;
+
+// Write a conditional statement that checks if the num is positive or negative
+if (num > 0) {
+    console.log("The number is positive.");
+} else if (num < 0) {
+    console.log("The number is negative.");
+} else {
+    console.log("The number is zero.");
+}
+
+// Change the value of the variable
+num = -5;
+
+// Run the conditional again
+if (num > 0) {
+    console.log("The number is positive.");
+} else if (num < 0) {
+    console.log("The number is negative.");
+} else {
+    console.log("The number is zero.");
+}
+
+// Conditional for granting access based on age
+let age = 20;
+if (age >= 18) {
+    console.log("Access Granted");
+} else {
+    console.log("Access Denied");
+}
